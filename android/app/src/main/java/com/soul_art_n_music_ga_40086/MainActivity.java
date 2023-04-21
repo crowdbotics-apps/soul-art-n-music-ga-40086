7cb1da3d84e3d49152ec18675d922a40bdf87acd
Crowdbotics <team@crowdbotics.com>
2023-04-21 12:17:48 +0000

    Initial commit.

---
+++ android/app/src/main/java/com/soul_art_n_music_ga_40086/MainActivity.java
@@ -0,0 +1,15 @@
+package com.soul_art_n_music_ga_40086;
+
+import com.facebook.react.ReactActivity;
+
+public class MainActivity extends ReactActivity {
+
+  /**
+   * Returns the name of the main component registered from JavaScript. This is used to schedule
+   * rendering of the component.
+   */
+  @Override
+  protected String getMainComponentName() {
+    return "soul_art_n_music_ga_40086";
+  }
+}
---
+++ options/options.js
@@ -0,0 +1,12 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "soul-art-n-music-ga-40086",
+  url: "https://soul-art-n-music-ga-40086.botics.co",
+  api: "https://soul-art-n-music-ga-40086.botics.co/api/v1"
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
