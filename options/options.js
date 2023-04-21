3954591e29cb205d6943bae8885abddff5a55bd0
jessica Cosimeno <soulkgz.enterprise@gmail.com>
2023-04-21 08:41:34 -0400

    [Crowdbotics Platform Commit]
    Crowdbotics Studio: Update app screens

--- options/options.js
+++ options/options.js
@@ -1,12 +1,11 @@
-import { StyleSheet } from "react-native"
-
 export const globalOptions = {
-  name: "soul-art-n-music-ga-40086",
-  url: "https://soul-art-n-music-ga-40086.botics.co",
-  api: "https://soul-art-n-music-ga-40086.botics.co/api/v1"
-}
+  "name": "soul-art-n-music-ga-40086",
+  "url": "https://soul-art-n-music-ga-40086.botics.co",
+  "api": "https://soul-art-n-music-ga-40086.botics.co/api/v1",
+  "initialRoute": "Untitled1"
+};
 export const modulesOptions = {
   "@modules/app-menu": {
     copy: "Routes available!"
   }
-}
+};
